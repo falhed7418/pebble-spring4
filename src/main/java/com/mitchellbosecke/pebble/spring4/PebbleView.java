ab8e69a75537fff18d15ed67399d58eaa398a771
Eric Bussieres <erbussieres@gmail.com>
2016-12-18 21:23:18 -0500

    Refactor and add test for french resource bundle

--- src/main/java/com/mitchellbosecke/pebble/spring4/PebbleView.java
+++ src/main/java/com/mitchellbosecke/pebble/spring4/PebbleView.java
@@ -7,6 +7,7 @@
 package com.mitchellbosecke.pebble.spring4;
 
 import com.mitchellbosecke.pebble.PebbleEngine;
+import com.mitchellbosecke.pebble.error.PebbleException;
 import com.mitchellbosecke.pebble.spring4.context.Beans;
 import com.mitchellbosecke.pebble.template.PebbleTemplate;
 
@@ -15,9 +16,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.support.RequestContextUtils;
 import org.springframework.web.servlet.view.AbstractTemplateView;
 
+import java.io.IOException;
 import java.io.Writer;
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.Locale;
 import java.util.Map;
 
@@ -27,15 +27,11 @@ import javax.servlet.http.HttpServletResponse;
 public class PebbleView extends AbstractTemplateView {
 
   public static final String REQUEST_VARIABLE_NAME = "request";
-
   public static final String RESPONSE_VARIABLE_NAME = "response";
-
   public static final String SESSION_VARIABLE_NAME = "session";
 
   private static final String BEANS_VARIABLE_NAME = "beans";
-
-  private static final int NANOS_IN_SECOND = 1000000;
-
+  private static final int NANO_PER_SECOND = 1000000;
   /**
    * <p>
    * TIMER logger. This logger will output the time required for executing
@@ -43,68 +39,74 @@ public class PebbleView extends AbstractTemplateView {
    * </p>
    * <p>
    * The value of this constant is
-   * <tt>com.mitchellbosecke.pebble.spring.PebbleView.TIMER</tt>. This allows
+   * <tt>com.mitchellbosecke.pebble.spring.PebbleView.timer</tt>. This allows
    * you to set a specific configuration and/or appenders for timing info at
    * your logging system configuration.
    * </p>
    */
   private static final Logger TIMER_LOGGER = LoggerFactory.getLogger(PebbleView.class.getName() + ".timer");
 
-  private String characterEncoding = "UTF-8";
-
-  private PebbleEngine engine;
-
+  private String characterEncoding;
+  private PebbleEngine pebbleEngine;
   private String templateName;
+
+  @Override
+  protected void renderMergedTemplateModel(Map<String, Object> model, HttpServletRequest request,
+                                           HttpServletResponse response) throws Exception {
+    long startNanoTime = System.nanoTime();
+
+    this.setCharacterEncoding(response);
+    this.addVariablesToModel(model, request, response);
+    this.evaluateTemplate(model, request, response);
+    this.logElapsedTime(startNanoTime, request);
+  }
+
+  private void setCharacterEncoding(HttpServletResponse response) {
+    if (this.characterEncoding != null) {
+      response.setCharacterEncoding(this.characterEncoding);
+    }
+  }
+
+  private void addVariablesToModel(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) {
+    model.put(BEANS_VARIABLE_NAME, new Beans(this.getApplicationContext()));
+    model.put(REQUEST_VARIABLE_NAME, request);
+    model.put(RESPONSE_VARIABLE_NAME, response);
+    model.put(SESSION_VARIABLE_NAME, request.getSession(false));
+  }
+
+  private void evaluateTemplate(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws IOException, PebbleException {
+    Locale locale = RequestContextUtils.getLocale(request);
+
+    Writer writer = response.getWriter();
+    try {
+      PebbleTemplate template = this.pebbleEngine.getTemplate(this.templateName);
+      template.evaluate(writer, model, locale);
+    } finally {
+      writer.flush();
+    }
+  }
+
+  private void logElapsedTime(long startNanoTime, HttpServletRequest request) {
+    if (TIMER_LOGGER.isDebugEnabled()) {
+      Locale locale = RequestContextUtils.getLocale(request);
+      long endNanoTime = System.nanoTime();
+
+      long elapsed = endNanoTime - startNanoTime;
+      long elapsedMs = elapsed / NANO_PER_SECOND;
+      TIMER_LOGGER.debug("Pebble template \"{}\" with locale {} processed in {} nanoseconds (approx. {}ms)",
+              this.templateName, locale, elapsed, elapsedMs);
+    }
+  }
 
   public void setCharacterEncoding(String characterEncoding) {
     this.characterEncoding = characterEncoding;
   }
 
-  public void setPebbleEngine(PebbleEngine engine) {
-    this.engine = engine;
+  public void setPebbleEngine(PebbleEngine pebbleEngine) {
+    this.pebbleEngine = pebbleEngine;
   }
 
   public void setTemplateName(String name) {
     this.templateName = name;
-  }
-
-  @Override
-  protected void renderMergedTemplateModel(Map<String, Object> model, HttpServletRequest request,
-                                           HttpServletResponse response) throws Exception {
-
-    response.setCharacterEncoding(this.characterEncoding);
-
-    long startNanos = System.nanoTime();
-
-    PebbleTemplate template = this.engine.getTemplate(this.templateName);
-
-    // Add beans context
-    model.put(BEANS_VARIABLE_NAME, new Beans(this.getApplicationContext()));
-
-    // Add request & response
-    model.put(REQUEST_VARIABLE_NAME, request);
-    model.put(RESPONSE_VARIABLE_NAME, response);
-
-    // Add session
-    model.put(SESSION_VARIABLE_NAME, request.getSession(false));
-
-    // Locale
-    Locale locale = RequestContextUtils.getLocale(request);
-
-    final Writer writer = response.getWriter();
-    try {
-      template.evaluate(writer, model, locale);
-    } finally {
-      writer.flush();
-    }
-
-    if (TIMER_LOGGER.isDebugEnabled()) {
-      long endNanos = System.nanoTime();
-
-      BigDecimal elapsed = BigDecimal.valueOf(endNanos - startNanos);
-      BigDecimal elapsedMs = elapsed.divide(BigDecimal.valueOf(NANOS_IN_SECOND), RoundingMode.HALF_UP);
-      TIMER_LOGGER.debug("Pebble template \"{}\" with locale {} processed in {} nanoseconds (approx. {}ms)",
-          this.templateName, locale, elapsed, elapsedMs);
-    }
   }
 }
--- src/main/java/com/mitchellbosecke/pebble/spring4/util/ViewUtils.java
+++ src/main/java/com/mitchellbosecke/pebble/spring4/util/ViewUtils.java
@@ -7,37 +7,13 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
-/**
- * Utility class for view
- *
- * @author Eric Bussieres
- */
 public class ViewUtils {
-
-  /**
-   * Deactivate constructor
-   */
   private ViewUtils() {
   }
 
-  /**
-   * Get current http request
-   *
-   * @return http request
-   */
   public static HttpServletRequest getRequest() {
     ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
     return attr.getRequest();
-  }
-
-  /**
-   * Get current http session
-   *
-   * @return http session
-   */
-  public static HttpSession getSession() {
-    return getRequest().getSession(true);
   }
 }
